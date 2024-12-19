eef17f40e3da10e89ae69f6dce9e51873dc97bba
jdmogollono <jdiegomogollon@outlook.com>
2024-12-19 15:51:03 -0500

    evaluation
    - Se toman los scripts "model_map.py" "eda.py" y se reescriben en "visor.ipynb" "eda.ipynb".
    - Se creó el archivo api.ipynb.
    - Se eliminan archivos .py y se dejan únicamente los archivos .ipynb.

--- scripts/eda/main.py
+++
@@ -1,5 +0,0 @@
-#Main principal del eda
-from eda import eda
-
-if __name__ == "__main__":
-    eda()
--- scripts/evaluation/main.py
+++
@@ -1,5 +0,0 @@
-#Main principal del eda
-from evaluation import model_map
-
-if __name__ == "__main__":
-    model_map()
